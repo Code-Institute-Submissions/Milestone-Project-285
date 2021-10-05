2f0d2918aa1481ef518eca33e4b044484e4b4c00
sanjaysanghera <sanjay.sanghera@gmail.com>
2021-10-05 10:38:25 +0000

    Added secret key to enviroment variables

--- pacific_plants/settings.py
+++ pacific_plants/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kayaaq^3bce5ien#x3l5=h&a@tg$yn01=-hyzy&nq8l%vlh0w+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
